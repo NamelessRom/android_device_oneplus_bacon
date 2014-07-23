93a1e58ca05f2eaf70675284f0773765f0ce6063
Evisceration <eviscerationls@gmail.com>
2014-07-23 19:17:17 +0200

    nameless bringup
    Change-Id: I5cf2f7ece6f553950bdf50a8416f34eacda61e73

--- nameless_bacon.mk
+++ nameless_bacon.mk
@@ -22,16 +22,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/nameless/config/apns.mk)
+
+# Inherit some common Nameless stuff
+$(call inherit-product, vendor/nameless/config/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_NAME := cm_bacon
+PRODUCT_NAME := nameless_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := One
