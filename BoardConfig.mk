d659d25dfcc73397fc82caf5cf99133f6570a993
Evisceration <eviscerationls@gmail.com>
2014-07-26 00:55:14 +0200

    use gcc 4.8
    Change-Id: I3d150dc1bfa84fe0e4a7c5b43a817c9872062eb6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@
 # Inherit from MSM8974 common
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
+# Use gcc 4.8
+TARGET_GCC_VERSION_EXP := 4.8
+
 # Kernel
 TARGET_KERNEL_CONFIG := custom_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
